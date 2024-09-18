ae773a13e6e2a2397cb34e23f2efcb70e319fb00
maliha-afk <24amaliha@gmail.com>
2024-09-18 22:31:06 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,46 @@
+#my_list= ["Abita", 35.6,1]
+
+friends= ["Tahmid Bhaiya","Zaheen Bhaiya","Ryan","Atiya","Ayesha"]
+
+#marks=[55.2,55.6,35.5,60.5,75.5]
+
+numbers=[3,4,7,1,5]
+
+#b_list=[True,False,True,False,True]
+
+print(friends[0])
+
+#Slice
+print(friends[0:4])
+
+#append
+print(friends)
+friends.append("Bani")
+
+#insert
+friends.insert(0,"Toya")
+print(friends)
+
+#pop remove
+friends.pop()
+print(friends)
+
+#remove
+friends.remove("Atiya")
+print(friends)
+
+#clear
+#friends.clear()
+#print(friends)
+
+#sort
+print(numbers)
+numbers.sort()
+print(numbers)
+
+#length
+print(len(friends))
+
+#reverse
+friends.reverse()
+print(friends)
